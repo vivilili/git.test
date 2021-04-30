09d18df15e237e2177f5ccb5edeaac5125fa085b
liujiawei <liujiaweiyo@163.com>
2021-04-30 15:34:46 +0800

    push commit2

--- src/main/java/com/liujiawei/git/GitTest.java
+++ src/main/java/com/liujiawei/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master git");
         System.out.println("push git");
+        System.out.println("push git1");
     }
 }
